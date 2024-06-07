af3de1434d1e89104e95558140792af797432c1c
jhonuru <jhon.urueta@misena.edu.co>
2024-06-07 12:29:31 -0500

    Avance 2

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -2,10 +2,146 @@
 .curso-main-container.pb-3
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
-    .titulo-principal.color-acento-contenido
+    .titulo-principal.color-acento-contenido(data-aos="flip-up")
       .titulo-principal__numero
         span 1
       h1 Correo electrónico
+    .row.bg-c4
+      .col-12
+        .px-5
+          .row.mb-4
+            .col-lg-4.mb-4.mb-lg-0
+              figure(data-aos="fade-right")
+                img(src='@/assets/curso/tema1/img2.png', style="width:350px" alt='').m-auto
+            .col-lg-8
+              p(data-aos="fade-left") El correo electrónico es un medio de comunicación asincrónico que ha tomado mucha importancia en los últimos años.
+              .row.mb-3
+                .col-12
+                  .p-4.bg-c3.shadow(data-aos="fade-down")
+                    .row.align-items-center
+                      .col.order-2.order-md-1
+                        p.mb-0 Las empresas, las entidades educativas, entre otras, disponen de sus cuentas de correo institucionales, que no solamente sirven para enviar información de forma oficial, sino que también permiten acceso a otro tipo de servicios y de beneficios.
+                      .col-md-auto.mb-4.mb-lg-0.order-1.order-md-2
+                        figure
+                          img(src='@/assets/curso/tema1/img1.svg', style="width:80px" alt='').m-auto
+              .row.mb-3(data-aos="fade-left")
+                .col-auto.d-none.d-md-block
+                  figure.mt-1
+                    img(src='@/assets/curso/tema1/img2.svg', style="width:80px" alt='').m-auto
+                .col
+                  p.mb-0 Hoy en día, es común que las empresas y diferentes organizaciones, soliciten un correo electrónico dentro de los datos de contacto de las personas, para enviarles información; también ocurre lo mismo con diligencias como fórmulas médicas y trámites legales.
+              p.mb-0(data-aos="fade-left") El correo electrónico se puede utilizar para cantidad de transacciones como:
+          .row.justify-content-center(data-aos="fade-up")
+            .col-8.col-md-6.col-lg-auto.mb-4.mb-xl-0
+              .p-3.bg-c3.shadow
+                .row.align-items-center 
+                  .col-auto 
+                    figure
+                      img(src='@/assets/curso/tema1/img3.svg', style="width:50px" alt='').m-auto
+                  .col 
+                    p.mb-0 Documentos
+            .col-8.col-md-6.col-lg-auto.mb-4.mb-xl-0
+              .p-3.bg-c3.shadow
+                .row.align-items-center 
+                  .col-auto 
+                    figure
+                      img(src='@/assets/curso/tema1/img4.svg', style="width:50px" alt='').m-auto
+                  .col 
+                    p.mb-0 Notificaciones
+            .col-8.col-md-6.col-lg-auto.mb-4.mb-xl-0
+              .p-3.bg-c3.shadow
+                .row.align-items-center 
+                  .col-auto 
+                    figure
+                      img(src='@/assets/curso/tema1/img5.svg', style="width:50px" alt='').m-auto
+                  .col 
+                    p.mb-0 Ubicación
+            .col-8.col-md-6.col-lg-auto.mb-4.mb-xl-0
+              .p-3.bg-c3.shadow
+                .row.align-items-center 
+                  .col-auto 
+                    figure
+                      img(src='@/assets/curso/tema1/img6.svg', style="width:50px" alt='').m-auto
+                  .col 
+                    p.mb-0 Pagos
+            .col-8.col-md-6.col-lg-auto.mb-4.mb-xl-0
+              .p-3.bg-c3.shadow
+                .row.align-items-center 
+                  .col-auto 
+                    figure
+                      img(src='@/assets/curso/tema1/img7.svg', style="width:50px" alt='').m-auto
+                  .col 
+                    p.mb-0 Negocios
+            .col-8.col-md-6.col-lg-auto.mb-4.mb-xl-0
+              .p-3.bg-c3.shadow
+                .row.align-items-center 
+                  .col-auto 
+                    figure
+                      img(src='@/assets/curso/tema1/img8.svg', style="width:50px" alt='').m-auto
+                  .col 
+                    p.mb-0 Eventos
+          Separador 
+          #t_1_1
+          .titulo-segundo.color-acento-contenido
+            h2 1.1 Cuentas y sincronización en equipos de cómputo
+          .row.mb-4
+            .col-12.bg-c5
+              .row.align-items-center.justify-content-center
+                .col-auto.px-0.d-none.d-md-block
+                  figure
+                    img(src='@/assets/curso/tema1/img10.svg', style="width:300px" alt='').m-auto
+                .col(data-aos="fade-left")
+                  .p-4.p-md-0
+                    p.mb-0 #[b.txt-c2 ¿Cómo vincular una cuenta?]
+                    p #[b.txt-c2 ¿De qué forma se puede aprovechar?]
+                    p.text-white.mb-0 Cuando se vincula la cuenta de correo electrónico con los navegadores y las aplicaciones, se tiene acceso a diversos servicios.
+    .row.mb-5
+      .col-lg-8.order-2.order-lg-1(data-aos="fade-right")
+        p Cuando se vincula la cuenta de correo electrónico con los navegadores y las aplicaciones, se tiene acceso a diversos servicios.
+        .row 
+          .col-auto
+            figure.mt-1
+              img(src='@/assets/curso/tema1/img2.svg', style="width:80px" alt='').m-auto
+          .col 
+            p.mb-0 Lo invitamos a consultar el siguiente video, donde conocerá cómo crear una cuenta de correo electrónico y los servicios a los que puede acceder, al vincularla a diferentes aplicaciones o navegadores.
+      .col-lg-4.mb-4.mb-lg-0.order-1.order-lg-2(data-aos="fade-left")
+        figure
+          img(src='@/assets/curso/tema1/img11.svg', style="width:350px" alt='').m-auto
+    .row.bg-c6
+      .col-12
+        .px-5
+          figure(data-aos="fade-right")
+            .video
+              iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+          Separador
+          #t_1_2
+          .titulo-segundo.color-acento-contenido
+            h2 1.2 Cuentas y sincronización en otros dispositivos
+          .row.align-items-center.mb-5
+            .col-lg-3.mb-4.mb-lg-0
+              figure(data-aos="fade-right")
+                img(src='@/assets/curso/tema1/img4.png', style="width:280px" alt='').m-auto
+            .col-lg-9
+              .row.mb-3
+                .col-12
+                  .p-3.bg-c7(data-aos="fade-down")
+                    .row.align-items-center
+                      .col.order-2.order-md-1
+                        p.mb-0 Algunas personas acostumbran revisar su correo electrónico desde un computador, pero también son muchas las que lo hacen desde un teléfono móvil.
+                      .col-md-auto.mb-4.mb-md-0.order-1.order-md-2
+                        figure
+                          img(src='@/assets/curso/tema1/img12.svg', style="width:60px" alt='').m-auto
+              .row.mb-3(data-aos="fade-left")
+                .col-auto.d-none.d-md-block
+                  figure.mt-1
+                    img(src='@/assets/curso/tema1/img2.svg', style="width:80px" alt='').m-auto
+                .col 
+                  p.mb-0 Mantener el correo electrónico configurado en otro dispositivo, permite revisarlo desde cualquier lugar, siempre y cuando se cuente con acceso a Internet.
+              p.mb-0(data-aos="fade-down") El siguiente video enseña cómo realizar una sincronización de la cuenta de correo electrónico en un dispositivo móvil.
+          figure
+            .video(data-aos="fade-left")
+              iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+          
       
 </template>
 
